2078cf98ba806b1087e71e64351d22fa6e128249
Rokaya Nasr <rokanasr60@gmail.com>
2024-10-17 03:41:58 +0300

    initial design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {faPlus, faEllipsis, faCheckCircle} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'todo';
+  faPlus = faPlus;
+  faEllipsis = faEllipsis;
+  faCheckCircle = faCheckCircle;
 }
